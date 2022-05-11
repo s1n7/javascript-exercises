62bd011e01e387deb963b1bcb831b4cf7e2887a4
s1n7 <luisbruemmer@gmail.com>
2022-05-11 17:56:03 +0200

    Complete 03_reverseString task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    const reverseArray = [];
+    for(let i = 0; i < stringArray.length; i++) {
+        reverseArray[stringArray.length-1-i] = stringArray[i];
+    }
 
+    return reverseArray.join("");
 };
 
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
