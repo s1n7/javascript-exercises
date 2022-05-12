66e03249e1ca9096d98e32c454ed4dda05e8193a
s1n7 <luisbruemmer@gmail.com>
2022-05-12 18:22:21 +0200

    Make task work for 1 parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === remove) {
+            array.splice(i, 1);
+        }
+    }
+
+    return array;
 };
+
+console.log(removeFromArray([1,2,3,4], 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
