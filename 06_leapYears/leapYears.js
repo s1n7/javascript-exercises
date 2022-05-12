2850d7b19d9009cf8a2a5d90120b974df84ac61c
s1n7 <luisbruemmer@gmail.com>
2022-05-12 21:00:14 +0200

    Start 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 !== 0) {
+        return false;
+    }
 
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        }
+        return false;
+    }
+    return true;
 };
-
+console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
