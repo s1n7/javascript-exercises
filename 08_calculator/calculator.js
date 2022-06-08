0c9d2f914bb886a54aeb68fa964d7c7816b7b771
s1n7 <luisbruemmer@gmail.com>
2022-06-08 14:18:58 +0200

    Complete 08_calculator task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for(a of array) {
+    result += a;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(a of array) {
+    result *= a;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 1 || n === 0) {
+    return 1;
+  }
+  
+  let result = 1;
+  while (n > 1) {
+    result *= n;
+    n--
+  }
+  return result;
 };
 
 // Do not edit below this line
