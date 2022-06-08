76e853131779bff1fca629b62149e50afc0e2ce0
s1n7 <luisbruemmer@gmail.com>
2022-06-08 17:38:04 +0200

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n !== typeof Number) {
+        n = Number(n);
+    }
+    
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n === 0) {
+        return 0;
+    } else if (n === 1){
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 };
 
 // Do not edit below this line
