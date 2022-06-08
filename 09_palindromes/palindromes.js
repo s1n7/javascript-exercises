1781d977c68a8e7a8b891e3429b0ff79f02f3b23
s1n7 <luisbruemmer@gmail.com>
2022-06-08 17:26:19 +0200

    Complete 09_palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[!,.]/g, "").replace(/ /g, "").toLowerCase();
+    const reverse = word.split("").reverse().join("").toLowerCase();
+    if (word === reverse) {
+        return true;
+    } 
+    return false;
 };
 
 // Do not edit below this line
